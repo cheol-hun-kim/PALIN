30ce1e60a0f019289f5f485b2610f8e80689baae
cheol-hun-kim <1286orbital21@gmail.com>
2026-09-01 01:20:12 +0900

    Add files via upload

---
+++ static/static/sw.js
@@ -0,0 +1,12 @@
+// Self-Purging Service Worker
+self.addEventListener('install', e => self.skipWaiting());
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.registration.unregister())
+      .then(() => self.clients.claim())
+  );
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request));
+});
