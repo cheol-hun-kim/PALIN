7a3f75dc86367f340b4d657e12dc10f3519ffe13
cheol-hun-kim <1286orbital21@gmail.com>
2026-09-03 01:41:15 +0900

    Add files via upload

--- app/database.py
+++ app/database.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.pool import NullPool
